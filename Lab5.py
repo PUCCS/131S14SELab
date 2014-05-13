391fefbe355af043bca14f302d15d712ff387b07
rafael molina <raf.moli@gmail.com>
2014-05-12 20:56:40 -0700

    new binary conversion

--- Lab5.py
+++ Lab5.py
@@ -1,6 +1,6 @@
 W = input('Whole Part > ')
 I = int(W)
-#F = input('Fractional Part > ')
+
 # Whole number to binary.
 
 PositionOne = I % 2
@@ -19,6 +19,20 @@ P4 = c % 2
 
 print(int(P4),int(PositionThree),int(PositionTwo),int(PositionOne))
 
+
 # Fractional part to binary.
 
 F = input('Fractional Part > ')
+IF = float(F)
+# This section turns decimals into binary values.
+P1 = IF * 2
+P2 = P1 * 2
+P3 = P2 * 2
+P4F = P3 * 2
+
+Positions = (P1,P2,P3,P4)
+for i in Positions:
+    if i > 1:
+        print('1')
+    else:
+        print('0')
