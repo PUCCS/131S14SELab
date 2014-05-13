57ae43931cafd50c6e8f6e8202d0c166c3028a14
rafael molina <raf.moli@gmail.com>
2014-05-13 08:52:11 -0700

    Fixed fractions to binary

---
+++ FTB2.py
@@ -0,0 +1,48 @@
+W = input('Whole Part > ')
+I = int(W)
+
+# Whole number to binary.
+
+PositionOne = I % 2
+
+a = I / 2
+
+PositionTwo = a % 2
+
+b = a / 2
+
+PositionThree = b % 2
+
+c = b / 2
+
+P4 = c % 2
+
+print(int(P4),int(PositionThree),int(PositionTwo),int(PositionOne))
+
+# Fractional part to binary.
+
+F = input('Fractional Part > ')
+IF = float(F)
+
+# This section turns decimals into binary values.
+import math
+
+P1 = IF * 2
+Pos1 = math.modf(P1)
+PosOne = P1
+
+P2 = Pos1[0] * 2
+Pos2 = math.modf(P2)
+PosTwo = Pos2[0]
+
+P3 = Pos2[0] * 2
+Pos3 = math.modf(P3)
+PosThree = Pos3[0]
+
+Positions = (PosOne,PosTwo,PosThree)
+for i in Positions:
+#    print(i)
+    if i > 1:
+        print('1')
+    else:
+        print('0')
